f3802f7d5c91aea3ad6b7639b95df55d5cf63d59
JustinJefferson <justleland@gmail.com>
2020-02-19 22:14:00 -0500

    added Mechanics page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Lore from './pages/Lore';
+import Mechanics from './pages/Mechanics';
+import Monsters from './pages/Monsters';
+
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/"></Route>
-          <Route path="lore"></Route>
-          <Route path="monsters"></Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="lore">
+            <Lore />
+          </Route>
+          <Route path="mechanics">
+            <Mechanics />
+          </Route>
+          <Route path="monsters">
+            <Monsters />
+          </Route>
         </Switch>
       </Router>
     </div>
---
+++ src/pages/Mechanics.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Mechanics.css';
+
+const Mechanics = () => 
+    <div>
+        <h1>This is the Mechanics Page!</h1>
+    </div>
+
+export default Mechanics;
